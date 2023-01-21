d6f643c31373abc4508b44c5f7028d31dea3707b
Jarekszu2 <jarekszu2@onet.pl>
2023-01-21 20:27:57 +0100

    Zapis do pliku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true))) {
+            printWriter.println("Hallo World!");
+        } catch (IOException e) {
+            System.err.println("Error, unable to open this file and write to it.");
+        }
     }
 }
